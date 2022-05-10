4d685489c01f82ba81fdd08463bc9eafda7d7a53
Dylan Laufenberg <edev455@gmail.com>
2022-05-09 17:41:47 -0700

    Update groupers for GroupedCollection trait.

--- src/groupers.rs
+++ src/groupers.rs
@@ -1,41 +1,46 @@
-use crate::grouped_collection::*;
+//! A collection of helper methods for grouping strings into a [GroupedCollection].
+
+use crate::grouped_collections::*;
 use crate::matchers::*;
 use regex::Regex;
 
-///! A collection of associated functions to insert Strings into a GroupedCollection.
-
-impl GroupedCollection<String, String> {
+/// Provides helper methods for grouping strings into a [GroupedCollection].
+///
+/// Each method corresponds to a [matcher](crate::matchers).
+pub trait Groupers<List> {
     /// Groups a String according to its first `n` characters and adds it to the collection.
     ///
     /// # Examples
     ///
     /// ```
-    /// # use groupby::grouped_collection::*;
-    /// let mut coll = GroupedCollection::new();
+    /// use groupby::grouped_collections::*;
+    /// use groupby::groupers::Groupers;
+    /// use std::collections::HashMap;
+    ///
     /// let expected = vec!["kaledonia".to_string()];
-    /// coll.group_by_first_chars(expected[0].clone(), 4);
-    /// assert_eq!(Some(&expected), coll.get(&"kale".to_string()));
+    /// let mut map = HashMap::new();
+    /// map.group_by_first_chars(expected[0].clone(), 4);
+    ///
+    /// assert_eq!(Some(&expected), map.get(&"kale".to_string()));
     /// ```
-    pub fn group_by_first_chars(&mut self, line: String, n: usize) {
-        let key = match_first_n_chars(&line, n).to_string();
-        self.add(key, line);
-    }
+    fn group_by_first_chars(&mut self, line: String, n: usize);
 
     /// Groups a String according to its last `n` characters and adds it to the collection.
     ///
     /// # Examples
     ///
     /// ```
-    /// # use groupby::grouped_collection::*;
-    /// let mut coll = GroupedCollection::new();
+    /// use groupby::grouped_collections::*;
+    /// use groupby::groupers::Groupers;
+    /// use std::collections::BTreeMap;
+    ///
     /// let expected = vec!["Sally".to_string()];
-    /// coll.group_by_last_chars(expected[0].clone(), 4);
-    /// assert_eq!(Some(&expected), coll.get(&"ally".to_string()));
+    /// let mut map = BTreeMap::new();
+    /// map.group_by_last_chars(expected[0].clone(), 4);
+    ///
+    /// assert_eq!(Some(&expected), map.get(&"ally".to_string()));
     /// ```
-    pub fn group_by_last_chars(&mut self, line: String, n: usize) {
-        let key = match_last_n_chars(&line, n).to_string();
-        self.add(key, line);
-    }
+    fn group_by_last_chars(&mut self, line: String, n: usize);
 
     /// Groups a String according to the provided Regex and adds it to the collection.
     ///
@@ -44,21 +49,38 @@ impl GroupedCollection<String, String> {
     /// # Examples
     ///
     /// ```
-    /// # use groupby::grouped_collection::*;
-    /// # use regex::Regex;
-    /// let mut coll = GroupedCollection::new();
+    /// use groupby::grouped_collections::*;
+    /// use groupby::groupers::Groupers;
+    /// use regex::Regex;
+    /// use std::collections::HashMap;
+    ///
     /// let expected = vec!["Nineteen99".to_string()];
     /// let regex = Regex::new(r"\d+").unwrap();
-    /// coll.group_by_regex(expected[0].clone(), &regex);
-    /// assert_eq!(Some(&expected), coll.get(&"99".to_string()));
+    /// let mut map = HashMap::new();
+    /// map.group_by_regex(expected[0].clone(), &regex);
+    ///
+    /// assert_eq!(Some(&expected), map.get(&"99".to_string()));
     /// ```
-    pub fn group_by_regex(&mut self, line: String, regex: &Regex) {
-        let key = match match_regex(&line, &regex) {
-            Some(s) => s,
-            None => "",
-        }
-        .to_string();
+    fn group_by_regex(&mut self, line: String, regex: &Regex);
+}
 
+impl<'s, List, GC> Groupers<List> for GC
+where
+    List: 's,
+    GC: GroupedCollection<'s, String, String, List>,
+{
+    fn group_by_first_chars(&mut self, line: String, n: usize) {
+        let key = match_first_n_chars(&line, n).to_string();
+        self.add(key, line);
+    }
+
+    fn group_by_last_chars(&mut self, line: String, n: usize) {
+        let key = match_last_n_chars(&line, n).to_string();
+        self.add(key, line);
+    }
+
+    fn group_by_regex(&mut self, line: String, regex: &Regex) {
+        let key = match_regex(&line, regex).unwrap_or("").to_string();
         self.add(key, line);
     }
 }

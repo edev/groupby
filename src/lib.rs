db4ce0982a60184982e712ae9af4b562de8fac05
Dylan Laufenberg <edev455@gmail.com>
2022-05-10 14:59:45 -0700

    Add brief crate root docs. Add TODOs for later.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+//! Utilities for creating collections of values grouped by common keys.
+//!
+//! This crate provides utilities for grouping values by common keys. A common use case might be
+//! grouping values into equivalence classes and storing these groups in either a
+//! [std::collections::HashMap] or a [std::collections::BTreeMap].
+
+// TODO Expand documentation here once groupers and matchers are more mature:
+// - Follow the examples of major crates like Rand
+// - Add a description of the major products: GroupedCollection, groupers, matchers...
+// - Add a quickstart
+// - Link to benchmarks
+
 pub mod command_line;
 pub mod grouped_collections;
 pub mod groupers;

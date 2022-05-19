f401735637a4eb057ca1d1efafe5210803f9cd73
Dylan Laufenberg <edev455@gmail.com>
2022-05-19 01:49:27 -0700

    Improve RecordWriter tests.
    - Add MockWriter struct and use it to verify behavior.
    - Clean up tests in various ways.

--- src/command_line/run_command/record_writer.rs
+++ src/command_line/run_command/record_writer.rs
@@ -52,13 +52,46 @@ impl<'a, W: Write> RecordWriter<'a, W> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io;
+
+    #[derive(Clone, Debug, Eq, PartialEq)]
+    struct MockWriter {
+        received: String,
+        flushed: bool,
+    }
+
+    impl MockWriter {
+        fn new() -> Self {
+            MockWriter {
+                received: String::new(),
+                flushed: false,
+            }
+        }
+
+        fn check(&self, expected: &str, expected_flush: bool) {
+            assert_eq!(&self.received, expected);
+            assert_eq!(self.flushed, expected_flush);
+        }
+    }
+
+    impl Write for MockWriter {
+        fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+            self.received.push_str(&String::from_utf8_lossy(buf));
+            Ok(buf.len())
+        }
+
+        fn flush(&mut self) -> io::Result<()> {
+            self.flushed = true;
+            Ok(())
+        }
+    }
 
     mod new {
         use super::*;
 
         #[test]
         fn works() {
-            let v = vec![];
+            let v: Vec<u8> = vec![b'a', b'c'];
             let sep = b"foo";
             let writer = RecordWriter::new(v.clone(), sep);
             assert_eq!(writer.writer.into_inner().unwrap(), v);
@@ -71,20 +104,18 @@ mod tests {
 
         #[test]
         fn writes_with_separator_and_flushes() {
-            let mut writer = RecordWriter::new(vec![], b"hoo");
+            let mut writer = RecordWriter::new(MockWriter::new(), b"hoo");
             writer.write("boo");
-            assert_eq!(writer.writer.into_inner().unwrap(), b"boohoo");
+            writer.writer.into_inner().unwrap().check("boohoo", true);
         }
 
         #[test]
-        #[should_panic]
+        #[should_panic(expected = "WriteZero")]
         fn panics_if_write_fails() {
             let mut buf = [0, 0];
             let writer = &mut buf[0..2];
-            let mut writer = RecordWriter::new(writer, &[3, 4]);
+            let mut writer = RecordWriter::new(writer, b"\0\0");
             writer.write("ab");
-            drop(writer);
-            println!("{:?}", buf);
         }
     }
 
@@ -95,13 +126,12 @@ mod tests {
         fn writes_all_values_and_flushes() {
             let values = ["My", "dog", "ate", "my", "homework"];
             let sep = ",\t";
-            let mut buf = vec![];
 
-            let mut writer = RecordWriter::new(&mut buf, sep.as_bytes());
+            let mut writer = RecordWriter::new(MockWriter::new(), sep.as_bytes());
             writer.write_all(values.iter());
 
-            let expected: Vec<u8> = (values.join(sep) + sep).into_bytes();
-            assert_eq!(&expected, writer.writer.into_inner().unwrap());
+            let expected: String = values.join(sep) + sep;
+            writer.writer.into_inner().unwrap().check(&expected, true);
         }
     }
 
@@ -110,9 +140,6 @@ mod tests {
 
         #[test]
         fn moves_writer() {
-            // Nearly the same as write_all::writes_all_values_and_flushes(), except here we'll
-            // use writer.writer() instead of writer.writer.
-
             let values = ["My", "dog", "ate", "my", "homework"];
             let sep = ",\t";
             let mut buf = vec![];
@@ -121,6 +148,8 @@ mod tests {
             writer.write_all(values.iter());
 
             let expected: Vec<u8> = (values.join(sep) + sep).into_bytes();
+
+            // into_inner() will fail if writer() doesn't move RecordWriter::writer.
             assert_eq!(&expected, writer.writer().into_inner().unwrap());
         }
     }

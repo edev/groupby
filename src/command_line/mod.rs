1b708975b7031ee3d53e43da00af5f3be032ca55
Dylan Laufenberg <edev455@gmail.com>
2022-05-25 13:46:54 -0700

    Add overview of command_line module.

--- src/command_line/mod.rs
+++ src/command_line/mod.rs
@@ -1,4 +1,12 @@
 //! Toolkit for building command-line applications that use this library.
+//!
+//! # Examples
+//!
+//! The [groupby] binary offers a simple map of the top-level API for this module. If you need
+//! finer control or want to reuse specific components within the top-level methods here, this map
+//! should provide jumping-off points for further reading.
+//!
+//! [groupby]: https://github.com/edev/groupby/tree/master/src/bin/groupby.rs
 
 pub mod args;
 pub mod options;

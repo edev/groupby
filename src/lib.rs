1dc95e663aff876aa1bc2ab9c44a43fd8555453d
Dylan Laufenberg <edev455@gmail.com>
2022-05-26 21:05:51 -0700

    Add example to crate root.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,35 @@
 //! This crate provides utilities for grouping values by common keys. A common use case might be
 //! grouping values into equivalence classes and storing these groups in either a
 //! [std::collections::HashMap] or a [std::collections::BTreeMap].
+//!
+//! # Examples
+//!
+//! ```
+//! use groupby::grouped_collections::*;
+//! use groupby::groupers::string::*;
+//! use std::collections::BTreeMap;
+//!
+//! let values = ["Alphabet", "Alps", "Alpine", "Equine", "Equator", "Equivalence"]
+//!     .into_iter()
+//!     .map(|s| s.to_string());
+//!
+//! let mut map: BTreeMap<String, Vec<String>> = BTreeMap::new();
+//! for value in values {
+//!     map.group_by_first_chars(value, 3);
+//! }
+//!
+//! let expected_alps: Vec<String> = ["Alphabet", "Alps", "Alpine"]
+//!     .into_iter()
+//!     .map(|s| s.to_string())
+//!     .collect();
+//! assert_eq!(map.get(&"Alp".to_string()).unwrap(), &expected_alps);
+//!
+//! let expected_equs: Vec<String> = ["Equine", "Equator", "Equivalence"]
+//!     .into_iter()
+//!     .map(|s| s.to_string())
+//!     .collect();
+//! assert_eq!(map.get(&"Equ".to_string()).unwrap(), &expected_equs);
+//! ```
 
 // TODO Expand documentation here once groupers and matchers are more mature:
 // - Follow the examples of major crates like Rand

c6bdd3cf474baca4e7190a08fc0e59fbe6163356
Dylan Laufenberg <edev455@gmail.com>
2022-05-26 21:56:10 -0700

    Add command-line example to crate root.

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,9 @@
 //! grouping values into equivalence classes and storing these groups in either a
 //! [std::collections::HashMap] or a [std::collections::BTreeMap].
 //!
-//! # Examples
+//! # Example: grouping
+//!
+//! A basic example demonstrating what grouping means and how it works:
 //!
 //! ```
 //! use groupby::grouped_collections::*;
@@ -31,6 +33,58 @@
 //!     .map(|s| s.to_string())
 //!     .collect();
 //! assert_eq!(map.get(&"Equ".to_string()).unwrap(), &expected_equs);
+//! ```
+//!
+//! # Example: command-line processing
+//!
+//! A quick demonstration of the crate's command-line capabilities. Simulate a directory listing
+//! of files with structured names, group the file names, and count the files in each group using
+//! `wc`:
+//!
+//! ```
+//! use groupby::command_line;
+//! use groupby::command_line::options::*;
+//! use std::collections::BTreeMap;
+//!
+//! // A complete application might parse GroupByOptions from command-line options, e.g. by calling
+//! // command_line::parse(command_line::args()).
+//! let options = GroupByOptions {
+//!     input: InputOptions {
+//!         separator: Separator::Null,
+//!     },
+//!     grouping: GroupingSpecifier::FirstChars(6),
+//!     output: OutputOptions {
+//!         separator: Separator::Line,
+//!         only_group_names: false,
+//!         run_command: Some("wc -l".to_string()),
+//!     },
+//! };
+//!
+//! // The GroupedCollection we'll use. HashMap is also supported but doesn't preserve group order.
+//! let mut map = BTreeMap::new();
+//!
+//! // A complete application might use io::stdin().
+//! let simulated_input = "\
+//!     ecs440 class notes.tex\0\
+//!     ecs450 class notes.tex\0\
+//!     ecs450 study guide.pdf";
+//!
+//! // Split tokens by null characters, group them by class, and add them to the map.
+//! command_line::process_input(simulated_input.as_bytes(), &mut map, &options);
+//!
+//! // A complete application might use io::stdout().
+//! let mut output = Vec::new();
+//!
+//! // Run `wc -l` once for each group, pass the group's contents to the group's stdin, and collect
+//! // each command's stdout.
+//! command_line::output_results(&mut output, &map, &options);
+//!
+//! assert_eq!(String::from_utf8_lossy(&output),
+//! "ecs440:
+//! 1
+//!
+//! ecs450:
+//! 2\n\n");
 //! ```
 
 // TODO Expand documentation here once groupers and matchers are more mature:
